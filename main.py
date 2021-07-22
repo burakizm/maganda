33e8d3a84abe134c7c960a00d94e734c2dcc221b
intikatimi <84741553+intikatimi@users.noreply.github.com>
2021-07-22 12:10:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def button():
 async def _(client, message):
 	user = message.from_user # Kullanıcın Kimliğini Alalım
 
-	await message.reply_text(text="**Merhaba {}!**\n\n__Ben @intikamtimii Tarafından Yazılmış Eğlence Botuyum :) \nDoğruluk mu? Cesaret mi? Oyun Komutu => /dc".format(
+	await message.reply_text(text="**Merhaba {}!**\n\n__Ben İntikam Dc Eğlence Botuyum:) \nMusic Botumuzu Denediniz Mi @intikamtimiimusicbot Oyun Komutu => /dc".format(
 		user.mention, # Kullanıcı'nın Adı
 		),
 	disable_web_page_preview=True, # Etiketin Önizlemesi Olmaması İcin Kullanıyoruz
